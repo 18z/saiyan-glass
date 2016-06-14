2f03600cb0fbfefda90a5d0e9de5ada54d7386cc
18z <xspiritualx@gmail.com>
2016-06-14 15:36:57 +0800

    get url title

--- module/print_url.py
+++ module/print_url.py
@@ -1,7 +1,17 @@
+import urllib
+import BeautifulSoup
+
 from common.abstracts import Module
+
 
 class print_url(Module):
 
     def run(self, url):
 
-        print url
+        self.getUrlTitle(url)
+
+    def getUrlTitle(self, url):
+
+        soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(url))
+
+        print soup.title.string
